4c600f3b68925433b51cb60fe6900aaf488e84e2
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-03-17 18:15:24 +0000

    Fix navbar.

--- app/elm/Helpers/Style.elm
+++ app/elm/Helpers/Style.elm
@@ -5,7 +5,7 @@ import Model exposing (..)
 
 toggleNavClasses : Model -> List ( String, Bool )
 toggleNavClasses model =
-    [ ( "trans-y--320 trans-y-0-ns", not model.navbarOpen ), ( "trans-y-0", model.navbarOpen ) ]
+    [ ( "trans-y--350 trans-y-0-ns", not model.navbarOpen ), ( "trans-y-0", model.navbarOpen ) ]
 
 
 showAtResults : Model -> ( String, Bool )
