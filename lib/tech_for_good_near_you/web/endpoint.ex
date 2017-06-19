4618814136b41bec65d4100796a7439cc6263808
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-06-19 14:45:44 +0100

    Remove unused sockets boilerplate

--- lib/tech_for_good_near_you/web/endpoint.ex
+++ lib/tech_for_good_near_you/web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule TechForGoodNearYou.Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :tech_for_good_near_you
 
-  socket "/socket", TechForGoodNearYou.Web.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
@@ -14,7 +12,6 @@ defmodule TechForGoodNearYou.Web.Endpoint do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
