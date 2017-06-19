a0dcaabdb7b4551d3266fce364109ac1c368f216
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-06-19 14:02:21 +0100

    Fix elm_controller tests

--- test/tech_for_good_near_you/web/controllers/elm_controller_test.exs
+++ test/tech_for_good_near_you/web/controllers/elm_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TechForGoodNearYou.Web.ElmControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Tech for Good Near You!"
   end
 end
